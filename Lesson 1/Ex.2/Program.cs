c36077799a51837fa1ebaa0c3cd1ae4a5f2660a6
Vladimir.Kaydashov <kajdashov@gmail.com>
2023-03-07 23:38:06 +0300

    Add Ex.2

---
+++ Lesson 1/Ex.2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Ввод чисел
+Console.WriteLine("Введите 3 числа для определения максимального.");
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int c = int.Parse(Console.ReadLine());
+
+// Максимальное число
+int max = a;
+
+// Определение максимального числа
+if(b>max) max = b;
+if(c>max) max = c;
+
+// Вывод максимального числа
+Console.WriteLine($"Максимальное число = {max}");
