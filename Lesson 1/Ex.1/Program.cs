abaedc00cc16b2056d55cdcf5707ca69ca651a17
Vladimir.Kaydashov <kajdashov@gmail.com>
2023-03-07 23:21:23 +0300

    Add Ex.1

---
+++ Lesson 1/Ex.1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите 2 числа для определения максимального и минимального.");
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+// if(a > b){
+//     Console.Write($"Максимальное число {a}, минимальное число {b}");
+// }else{
+//     Console.Write($"Максимальное число {b}, минимальное число {a}");
+// }
+
+if(a > b) Console.Write($"Максимальное число {a}, минимальное число {b}");
+if(a < b) Console.Write($"Максимальное число {b}, минимальное число {a}");
+if(a == b) Console.Write($"Числа равны");
