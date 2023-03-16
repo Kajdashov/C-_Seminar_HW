807f33f889892ad2b28be22bb325f3049b895676
Vladimir.Kaydashov <kajdashov@gmail.com>
2023-03-16 20:37:34 +0300

    Add Ex.3

---
+++ Lesson 3/Ex.3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string text)
+{
+    System.Console.Write($"{text}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = Prompt("Введите число: ");
+
+for (int i = 1; i <= number; i++)
+{
+    int sum = (i * i * i);
+    System.Console.Write(sum + ", ");
+}
