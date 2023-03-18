9f281beeb6e251bea094f604a1aad862169eb228
Vladimir.Kaydashov <kajdashov@gmail.com>
2023-03-18 23:52:29 +0300

    Add Ex.3

---
+++ Lesson 4/Ex.3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (числа берете любые)
+
+int lenArray = Prompt("Введите длину массива: ");
+
+int[] Array = new int[lenArray];
+
+for(int i = 0; i < Array.Length; i++)
+{
+    Array[i] = new Random().Next(1,9);
+    System.Console.Write(Array[i] + " ");
+}
+
+int Prompt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
