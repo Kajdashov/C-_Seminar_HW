5c89a5e421c062e13036b9423454c3cf7bbe4488
Vladimir.Kaydashov <kajdashov@gmail.com>
2023-03-21 22:06:42 +0300

    Add Ex.1 on Lesson 5

---
+++ Lesson 5/Ex.1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+
+int[] array = GetRandomArray(12, 100, 999);
+Console.WriteLine($"[{String.Join(",", array)}]");
+System.Console.WriteLine(counNumber(array));
+
+
+
+
+// ------------------------------Метод вывода количества четных чисел------------------
+
+int counNumber(int[] Array)
+{
+    int count = 0;
+    // int count2 = 0;
+    for(int i = 0; i < Array.Length; i++ )
+    {
+        if(Array[i] % 2 == 0) count = count + 1;
+        // else count2++;
+    }
+
+   return count;
+
+}
+
+
+// -------------------------------Общий метод-------------------------------------------
+int[] GetRandomArray(int size, int minValue, int maxValue)
+            {
+                int[] result = new int[size];
+                for (int i = 0; i < size; i++)
+                {
+                    result[i] = new Random().Next(minValue, maxValue + 1);
+                }
+
+                return result;
+            }
