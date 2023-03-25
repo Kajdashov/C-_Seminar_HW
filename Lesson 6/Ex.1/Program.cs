2661443c7b664cea55ff05da3f8f1a8247a380ae
Vladimir.Kaydashov <kajdashov@gmail.com>
2023-03-25 13:15:39 +0300

    Add Lesson 6 and Lesson6/Ex.1

---
+++ Lesson 6/Ex.1/Program.cs
@@ -0,0 +1,27 @@
+﻿System.Console.Write("Введите размерность массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+int[] array = new int[size];
+
+int[] Array(int[] arr, int s)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"Введите {i + 1} элемент массива: ");
+        arr[i] = int.Parse(Console.ReadLine()!);
+    }
+    return arr;
+}
+
+int sort(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0) count++;
+    }
+    return count;
+}
+
+System.Console.WriteLine($"Введенный массива = [{String.Join(" , ", Array(array, size))}]");
+System.Console.WriteLine($"Чисел больше нуля в массиве -> {sort(array)}");
