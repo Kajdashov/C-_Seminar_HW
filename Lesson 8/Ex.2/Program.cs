197abd1aea9a4da5d5034144114e71bbb748a423
Vladimir.Kaydashov <kajdashov@gmail.com>
2023-04-04 22:29:27 +0300

    Add HW 8

---
+++ Lesson 8/Ex.2/Program.cs
@@ -0,0 +1,95 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// 1, 2, 3
+// 4, 6, 1
+// 2, 1, 6
+// 1 встречается 3 раза
+// 2 встречается 2 раз
+// 3 встречается 1 раз
+// 4 встречается 1 раз
+// 6 встречается 2 раза
+
+void Print2DArray(int[,] int2DArray)
+{
+
+    for (int i = 0; i < int2DArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < int2DArray.GetLength(1); j++)
+        {
+            Console.Write($"{int2DArray[i, j]} ");
+
+        }
+        Console.WriteLine();
+    }
+    Console.ForegroundColor = ConsoleColor.White;
+}
+
+int[,] Create2DimArray(int hight, int length, int min, int max)
+{
+    int[,] int2DArray = new int[hight, length];
+
+    for (int i = 0; i < hight; i++)
+    {
+        for (int j = 0; j < length; j++)
+        {
+            int2DArray[i, j] = new Random().Next(min, max);
+        }
+    }
+
+    return int2DArray;
+}
+
+bool isInArray(int value, int[,] int2DArray)
+{
+    foreach (int item in int2DArray)
+    {
+        if (item == value) return true;
+    }
+    return false;
+}
+
+int countArrayElement(int value, int[,]int2DArray)
+{
+    int counter = 0;
+    foreach (int item in int2DArray)
+    {
+        if (value == item) counter++;
+    }
+    return counter;
+}
+
+int[,] GetFrequencyArray(int[,] int2DArray)
+{
+    int[,] frequencyArray = new int[int2DArray.Length, 2];
+    int element = 0;
+    int zeroElement = 0;
+
+    foreach (int item in int2DArray)
+    {
+        if (!isInArray(item, frequencyArray) && item != 0)
+        {
+            frequencyArray[element, 0] = item;
+            frequencyArray[element, 1] = countArrayElement(item, int2DArray);
+        }
+        else if(item == 0 && zeroElement == 0)
+        {
+            frequencyArray[element, 1] = countArrayElement(item, int2DArray);
+            zeroElement++;
+        }
+        element++;
+    }
+    return frequencyArray;
+}
+
+void PrintFrequencyArray(int[,] frequencyArray)
+{
+    for (int i = 0; i < frequencyArray.GetLength(0); i++)
+    {
+        if(frequencyArray[i,1] != 0)
+            Console.WriteLine($"{frequencyArray[i,0]} встречается {frequencyArray[i,1]} раз");
+    }
+}
+
+int[,] myArray = Create2DimArray(3, 3, 1, 10);
+Print2DArray(myArray);
+int[,] myFrArray = GetFrequencyArray(myArray);
+PrintFrequencyArray(myFrArray);
---
+++ Lesson 8/Ex.4/Program.cs
@@ -0,0 +1,38 @@
+﻿// // Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// // Массив размером 2 x 2 x 2
+
+int[,,] array = new int[2, 2, 2];
+
+FillArray(array);
+PrintArray(array);
+
+void PrintArray(int [,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.WriteLine();
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+            System.Console.Write($"{array[i,j,k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+void FillArray(int[,,] array)
+{
+    int count = 10;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
